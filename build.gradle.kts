786eafd69bd91486bdb934deae15487ab616f867
KopylashTetiana <kopylashtetiana@gmail.com>
2023-05-08 21:28:01 +0300

    application.properties added, pstgresql dependensy added into build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation ("org.postgresql:postgresql")
 }
 
 tasks.withType<Test> {
